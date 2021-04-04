106f9cdf92772868d8a77794cb53bc61bb1c2f88
Narawish Sangsiriwut <narawish.s@ku.th>
2021-04-04 22:57:52 +0700

    guessing game add

--- app/sample.py
+++ app/sample.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from pymongo import MongoClient
 import os, json, redis
 
@@ -6,40 +6,62 @@ import os, json, redis
 application = Flask(__name__)
 
 # connect to MongoDB
-mongoClient = MongoClient('mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_AUTHDB'])
+mongoClient = MongoClient(
+    'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ[
+        'MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_AUTHDB'])
 db = mongoClient[os.environ['MONGODB_DATABASE']]
 
-# connect to Redis
-redisClient = redis.Redis(host=os.environ.get("REDIS_HOST", "localhost"), port=os.environ.get("REDIS_PORT", 6379), db=os.environ.get("REDIS_DB", 0))
+myCollection = db.game
 
-@application.route('/')
+
+@application.route('/', methods=['post', 'get'])
 def index():
-    return '<a href="http://localhost/sample">sample</a>'
+    myCollection.delete_many({})
+    return render_template("index.html")
 
-@application.route('/sample')
-def sample():
-    doc = db.test.find_one()
-    # return jsonify(doc)
-    body = '<div style="text-align:center;">'
-    body += '<h1>Python</h1>'
-    body += '<p>'
-    body += '<a target="_blank" href="https://flask.palletsprojects.com/en/1.1.x/quickstart/">Flask v1.1.x Quickstart</a>'
-    body += ' | '
-    body += '<a target="_blank" href="https://pymongo.readthedocs.io/en/stable/tutorial.html">PyMongo v3.11.2 Tutorial</a>'
-    body += ' | '
-    body += '<a target="_blank" href="https://github.com/andymccurdy/redis-py">redis-py v3.5.3 Git</a>'
-    body += '</p>'
-    body += '</div>'
-    body += '<h1>MongoDB</h1>'
-    body += '<pre>'
-    body += json.dumps(doc, indent=4)
-    body += '</pre>'
-    res = redisClient.set('Hello', 'World')
-    if res == True:
-      # Display MongoDB & Redis message.
-      body += '<h1>Redis</h1>'
-      body += 'Get Hello => '+redisClient.get('Hello').decode("utf-8")
-    return body
+
+@application.route('/game/start', methods=['post'])
+def initialize():
+    data = request.form['word']
+    answer = []
+    answer[:0] = data
+    hint = []
+    choice = []
+    for i in range(0, len(answer)):
+        hint.append("*")
+        if answer[i] not in choice:
+            choice.append(answer[i])
+    myCollection.insert_one({
+        "count": 0,
+        "answer": answer,
+        "choice": choice,
+        "hint": hint,
+        "guess": [],
+        "end": False
+    })
+    return render_template('start.html')
+
+
+@application.route('/game', methods=['post', 'get'])
+def play():
+    game = myCollection.find_one()
+    if request.method == 'POST' and len(game['answer']) != len(game['guess']):
+        data = request.form['choice']
+        my_query = {"answer": game['answer']}
+        answer = game['guess']
+        answer.append(data)
+        new_data = {"$set": {"guess": answer}}
+        myCollection.update_one(my_query, new_data)
+        if answer == game['answer']:
+            myCollection.update_one(my_query, {"$set": {"end": True}})
+    if len(game['answer']) == len(game['guess']) and game['answer'] != game['guess']:
+        my_query = {"answer": game['answer']}
+        count = game['count']
+        count += 1
+        myCollection.update_one(my_query, {"$set": {"count": count, "guess": []}})
+    game = myCollection.find_one()
+    return render_template("game.html", game=game)
+
 
 if __name__ == "__main__":
     ENVIRONMENT_DEBUG = os.environ.get("FLASK_DEBUG", True)
